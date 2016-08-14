c49164861206b061f5eba20525e9b2e40ed619d7
Frédéric DUPERIER <fduperier@gmail.com>
2016-08-14 12:02:52 +0200

    Migrating to Angular2 RC5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 
-import { AutoGrowDirective, UnlessDirective } from './shared';
-
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  directives: [ AutoGrowDirective, UnlessDirective ]
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
